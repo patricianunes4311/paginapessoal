ef1f5909ffb90baa7c91711bfb3917ab836ede68
Patrícia Gonçalves Nunes <patriciagoncalvesnunes844@gmail.com>
2022-05-03 21:34:18 -0300

    atualização

--- script/script.js
+++ script/script.js
@@ -1,58 +1,84 @@
-const checkForm= {
+const checkForm = {
     name: false,
-    email:false,
-    fone:false,
-    mensagem:false
+    email: false,
+    fone: false,
+    
 }
 
-document.getElementById('name').addEventListener('input', function(e){
+document.getElementById('name').addEventListener('input', function (e) {
     console.log(checkForm);
     const name = e.target.value;
-    if(name.length  < 1  || name.length > 50 ){
-    console.log('O nome inserido deve ter 1 à 50 caracteres');
-    document.getElementById('name-error').style.display = 'block';
-    checkForm = false;
-    }else{
-        document.getElementById('name-error').style.display = 'none';
-        checkForm = true;
+    if (name.length < 1 || name.length > 50) {
+        console.log('O nome inserido deve ter 1 à 50 caracteres');
+        document.getElementById('name-error').style.display = "block";
+        checkForm.name = false;
+    } else {
+        document.getElementById('name-error').style.display = "none";
+        checkForm.name = true;
     }
-}
-);
 
-document.getElementById('email').addEventListener('input',function(e){
+    enableButton();
+   
+});
+
+document.getElementById('email').addEventListener('input', function (e) {
     console.log(checkForm);
     const email = e.target.value;
-    if(validEmail(email)){ 
-        console.log('Neste campo só pode ser inserido email');  
-        document.getElementById('email-error').style.display='block'; 
-        checkForm = false;  
-    }else{        
-        document.getElementById('email-error').style.display='none';
-        checkForm = true;
+    if (validarEmail(email)) {
+        console.log('Neste campo só pode ser inserido email');
+        document.getElementById('email-error').style.display = "block";
+        checkForm.email = false;
+    } else {
+        document.getElementById('email-error').style.display = "none";
+        checkForm.email = true;
     }
 
+    enableButton();
+
+  
 });
 
-document.getElementById('fone').addEventListener('input',function(e){
+
+document.getElementById('fone').addEventListener('input', function (e) {
     console.log(checkForm);
     const fone = e.target.value;
-    if( validPhone (fone)){  
-    console.log('Neste campo deve ser inserido caracteres númericos');      
-    document.getElementById('fone-error').style.display='block';
-    checkForm = false;
-    }else{
-        document.getElementById('fone-error').style.display='none';
-        
+    if (validarNumero(fone)) {
+        document.getElementById('fone-error').style.display = "block";
+        checkForm.fone = false;
+    } else {
+        document.getElementById('fone-error').style.diplay = "none";
     }
+
+    
+    function validarNumero(str) {
+
+        let pattern = new RegExp('^((\\+1)?\\s?\\(\\d{3}\\)\\s?\\d{3}\\~\\d{4})?$');
+        return !pattern.test(str);
+
+    }
+    enableButton();
+
 });
 
 
-function validEmail(str){
-    let pattern = new RegExp("[a-zA-Z\s]+$");
+
+
+function validarEmail(str) {
+    let pattern = new RegExp('[^@]+@[^@]+\.[^@]+');
     return !pattern.test(str);
 }
 
-function validPhone (phone) {
-    var regex = new RegExp('^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$');
-    return regex.test(phone);
+function enableButton() {
+    const butcad = document.getElementById("button");
+    if (checkForm.productName &&
+        checkForm.productDescription &&
+        checkForm.productPrice &&
+        checkForm.urlProductImage) {
+        butcad.disabled = false;
+    } else {
+        butcad.disabled = true;
+    }
 }
+
+
+
